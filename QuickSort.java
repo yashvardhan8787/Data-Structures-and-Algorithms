cb452f367d25d6f9d1e7ed9542110478bd2ca34c
yashvardhan8787 <139737436+yashvardhan8787@users.noreply.github.com>
2025-03-04 21:40:15 +0530

    all QuickSort

--- QuickSort.java
+++ QuickSort.java
@@ -9,6 +9,7 @@ public class QuickSort {
         }
     }
 
+
     public int partition(int arr[] ,int  low ,int high){
         int pivot = arr[low] ;
         int i = low ;
@@ -28,18 +29,80 @@ public class QuickSort {
     }
 
     public void swap(int arr[], int  i , int j){
-        System.out.println("swaping "+arr[i] +" and  "+ arr[j]);
+//        System.out.println("swaping "+arr[i] +" and  "+ arr[j]);
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
 
+
+
+
+    public void quickSortByMid(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        //    Choose a pivot element (in this case I am choosing middle element as pivot)
+        int mid = (left + right) / 2;
+        int pivot = arr[mid];
+        //    Initialize left and right pointers at extremes.
+        int i = left, j = right;
+        while (i <= j) {
+            //   3. Find the first element to the left of the pivot which is greater than pivot.
+            while (arr[i] < pivot) {
+                i++;
+            }
+            //   4. Similarly find the first element to the right of the pivot which is smaller than pivot
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                //  5.  Swap elements found in 3 and 4.
+                swap(arr,i ,j);
+                i++;
+                j--;
+            }
+        } // Repeat 3,4,5 unless left >= right.
+
+        //    Repeat the whole thing for left and right subarray as pivot is now placed at its place.
+        quickSortByMid(arr, left, j);
+        quickSortByMid(arr, i, right);
+    }
+
+    public void quicksort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pivot = arr[right];
+        int partitionIndex = partition2(arr, left, right, pivot);
+
+        quicksort(arr, left, partitionIndex - 1);
+        quicksort(arr, partitionIndex + 1, right);
+    }
+
+    int partition2(int[] arr, int left, int right, int pivot) {
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
+    }
     public static void main(String[] args) {
         QuickSort qs = new QuickSort();
         int[] arr={4,6,2,5,7,9,3,1};
 //        System.out.print("array : [ ");
 //        for (int i :arr) {
+
 //            System.out.print(i+",");
 //        }
 //        System.out.println(" ]");
@@ -50,9 +113,13 @@ public class QuickSort {
 //            System.out.print(i+",");
 //        }
 //        System.out.println(" ]");
-        qs.sort(arr,0 ,7);
-        System.out.print("array : [ ");
-        for (int i :arr) {
+//        qs.sort(arr,0 ,7);
+
+//          qs.quickSortByMid(arr,0,7);
+//        qs.sort(arr,0,7);
+        qs.quicksort(arr,0,7);
+          System.out.print("array : [ ");
+                for (int i :arr) {
             System.out.print(i+",");
         }
         System.out.println(" ]");
